2007f0030a1a4aa9c964a2b2ecebff7e2147c07a
Darian Miller <darian@darianmiller.com>
2013-06-09 17:43:30 -0500

    URIEncodeElement + URIDecodeElement for percent encoding based on RFC 3986 list of Unreserved Characters.

--- Source/d5xStrings.pas
+++ Source/d5xStrings.pas
@@ -27,6 +27,9 @@ unit d5xStrings;
 
 interface
 
+type
+  TSysAnsiCharSet = set of AnsiChar;
+
   function LowString():Integer;
   function HighString(const pValue:String):Integer; overload;
   {$IFDEF STRING_IS_UNICODE}
@@ -38,8 +41,48 @@ interface
   function IsPrintableCharacter(const pChar:AnsiChar):Boolean; overload;
   {$ENDIF}
 
+  function CharInSet(C:AnsiChar; const CharSet:TSysAnsiCharSet):Boolean;
+
+  
+  {$IFDEF NODEF}{$REGION 'Documentation'}{$ENDIF}
+  /// <summary>
+  ///  Converts text into a form that contains only Unreserved Characters as per
+  ///  RFC 3986
+  /// </summary>
+  /// <remarks>
+  ///  Not intended for encoding a full URI, rather just individual eleements
+  ///  such as parameters.
+  /// </remarks>
+  {$IFDEF NODEF}{$ENDREGION}{$ENDIF}
+  function URIEncodeElement(const pText:AnsiString):AnsiString;
+
+
+  {$IFDEF NODEF}{$REGION 'Documentation'}{$ENDIF}
+  /// <summary>
+  ///  Decodes text that includes percent-encoded characters
+  /// </summary>
+  /// <remarks>
+  ///  URIDecodeElement decodes what it can, otherwise returns what was provided
+  ///  instead of generating an exception on invalid data.
+  /// </remarks>
+  {$IFDEF NODEF}{$ENDREGION}{$ENDIF}
+  function URIDecodeElement(const pText:AnsiString):AnsiString;
+
+
+const
+  EmptyAnsiString:AnsiString = '';
+  LowerCaseCharacters = ['a'..'z'];
+  UpperCaseCharacters = ['A'..'Z'];
+  CharacterDigits = ['0'..'9'];
+  AlphaCharacters = LowerCaseCharacters + UpperCaseCharacters;
+  AlphaNumericCharacters = AlphaCharacters + CharacterDigits;
+
+  Unreserved3986Characters = AlphaNumericCharacters + ['-','_','.','~'];  //RFC 3986 list of "Unreserved Characters"
+
 
 implementation
+uses
+  SysUtils;
 
 
 //1 for Legacy Delphi compilers
@@ -79,5 +122,101 @@ begin
   Result := (Ord(pChar) >= 32) and (Ord(pChar) <= 126);
 end;
 {$ENDIF}
+
+
+function CharInSet(C:AnsiChar; const CharSet:TSysAnsiCharSet):Boolean;
+begin
+  Result := C in CharSet;
+end;
+
+
+function URIEncodeElement(const pText:AnsiString):AnsiString;
+var
+  i:integer;
+begin
+  Result := '';
+
+  for i := LowString to HighString(pText) do
+  begin
+    if not CharInSet(pText[i], Unreserved3986Characters) then
+    begin
+      Result := Result + '%' + AnsiString(IntToHex(Ord(pText[i]), 2));
+    end
+    else
+    begin
+      Result := Result + pText[i];
+    end;
+  end;
+end;
+
+
+function URIDecodeElement(const pText:AnsiString):AnsiString;
+var
+  i:integer;
+  vEncodePosition:Integer;
+  vHexCode:AnsiString;
+begin
+  Result := '';
+  vHexCode := '';
+  vEncodePosition := 0;
+
+  for i := LowString to HighString(pText) do
+  begin
+    Case vEncodePosition of
+    0:
+      begin
+        if pText[i] = '%' then
+        begin
+          vEncodePosition := 1;
+        end
+        else
+        begin
+          Result := Result + pText[i];
+        end;
+      end;
+    1:
+      begin
+        if CharInSet(pText[i], AlphaNumericCharacters) then
+        begin
+          vHexCode := vHexCode + pText[i];
+          Inc(vEncodePosition);
+        end
+        else //err
+        begin
+          Result := Result + '%' + pText[i];
+          vEncodePosition := 0;
+          vHexCode := '';
+        end;
+      end;
+    2:
+      begin
+        if CharInSet(pText[i], AlphaNumericCharacters) then
+        begin
+          vHexCode := vHexCode + pText[i];
+          Result := Result + AnsiString(Chr(StrToInt(Format('$%s',[vHexCode]))));
+          vEncodePosition := 0;
+          vHexCode := '';
+        end
+        else //err
+        begin
+          Result := Result + '%' + vHexCode + pText[i];
+          vEncodePosition := 0;
+          vHexCode := '';
+        end;
+      end;
+    end;
+  end;
+
+  //Check for invalid characters on the tail end..
+  if vEncodePosition = 1 then
+  begin
+    Result := Result + '%';
+  end
+  else if vEncodePosition = 2 then
+  begin
+    Result := Result + '%' + vHexCode;
+  end;
+end;
+
 
 end.
